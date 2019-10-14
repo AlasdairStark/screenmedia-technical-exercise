4ad9abe37b039619b2adea2cda4e10cfded9cafc
Alasdair Stark <me@alasdairstark.com>
2019-10-14 21:03:11 +1300

    Simple view and viewmodel for listing notes

---
+++ Screenmedia.ToDo.Web/Models/ToDoNotes/ToDoNotesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Screenmedia.ToDo.Web.Models.ToDoNotes
+{
+    public class ToDoNotesViewModel
+    {
+        public IList<ToDoNoteViewModel> ToDoNotes { get; } = new List<ToDoNoteViewModel>();
+    }
+}

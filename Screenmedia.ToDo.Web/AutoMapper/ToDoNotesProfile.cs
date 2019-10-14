83b9fcb1174d8ff14fbd112f2d417e0d5212163b
Alasdair Stark <me@alasdairstark.com>
2019-10-14 21:25:13 +1300

    Added AutoMapper

---
+++ Screenmedia.ToDo.Web/AutoMapper/ToDoNotesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Screenmedia.ToDo.Web.Data.Models;
+using Screenmedia.ToDo.Web.Models.ToDoNotes;
+
+namespace Screenmedia.ToDo.Web.AutoMapper
+{
+    public class ToDoNotesProfile : Profile
+    {
+        public ToDoNotesProfile()
+        {
+            CreateMap<ToDoNote, ToDoNoteViewModel>();
+        }
+    }
+}
